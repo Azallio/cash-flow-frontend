a7e905c60bef85d92dc411f1097a68051248ab70
Azallio <allio.all.in.one@gmail.com>
2026-06-29 09:37:34 +0300

    fix(root): add links to tags in head

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -1,34 +1,41 @@
-import { MantineProvider } from '@mantine/core'
+import { ColorSchemeScript, MantineProvider } from '@mantine/core'
 import '@mantine/core/styles.css'
 import { ThemeProvider } from '@shared/service/provider/theme-provider.component'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Outlet, Scripts, ScrollRestoration } from 'react-router'
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from 'react-router'
 import type { Route } from './+types/root'
 import './app.css'
 
 export const links: Route.LinksFunction = () => [{ rel: 'icon', href: '/favicon.svg', type: 'image/png' }]
 
+// ← Вынести за пределы компонента
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+})
+
 export function Layout(props: React.PropsWithChildren) {
   const { children } = props
 
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        retry: false,
-        staleTime: 1000 * 60 * 5, // 5 minutes
-      },
-    },
-  })
-
   return (
     <html suppressHydrationWarning>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <ColorSchemeScript defaultColorScheme="dark" />
+        <Meta />
+        <Links />
+      </head>
       <body>
         <QueryClientProvider client={queryClient}>
           <MantineProvider defaultColorScheme="dark">
             <ThemeProvider defaultMode="dark">{children}</ThemeProvider>
           </MantineProvider>
         </QueryClientProvider>
-
         <ScrollRestoration />
         <Scripts />
       </body>
