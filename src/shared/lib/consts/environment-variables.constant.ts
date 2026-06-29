6d82a51fec41c1bc85c102affa82913264874b28
Azallio <allio.all.in.one@gmail.com>
2026-06-29 09:50:57 +0300

    fix(nginx): add proxy for backend

--- src/shared/lib/consts/environment-variables.constant.ts
+++ src/shared/lib/consts/environment-variables.constant.ts
@@ -1,1 +1,4 @@
-export const BASE_API_URL = import.meta.env.VITE_BASE_API_URL
+export const BASE_API_URL =
+  typeof window === 'undefined'
+    ? (process.env.VITE_BASE_API_URL ?? '/api') // SSR — Node.js
+    : (import.meta.env.VITE_BASE_API_URL ?? '/api') // Клиент — браузер
